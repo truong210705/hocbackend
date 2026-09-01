a9a96a8093d18baefc117fc839152d14ee3ca36f
truong210705 <truongdeptrai2107@gmail.com>
2026-09-01 22:42:02 +0700

    làm router trang sản phẩm và giao diện

---
+++ controllers/admin/product.controller.js
@@ -0,0 +1,5 @@
+module.exports.index = (req, res) => {
+  res.render("admin/page/product/index", {
+    title: "trang sanr pham",
+  });
+};
--- router/admin/index.route.js
+++ router/admin/index.route.js
@@ -1,5 +1,7 @@
 const dashboard = require("./dashboard.route");
+const product = require("./product.route");
 const system = require("../../config/system");
 module.exports = (app) => {
   app.use(system.prefixadmin + "/dashboard", dashboard);
+  app.use(system.prefixadmin + "/products", product);
 };
